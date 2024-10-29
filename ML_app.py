e0922b2fd98e39f5bb3b1713c3eb3d59c7dac0f4
emilyk <emily.g.kruse@gmail.com>
2024-10-29 11:33:51 -0400

    fix errors

--- ML_app.py
+++ ML_app.py
@@ -17,7 +17,7 @@ def main():
 
   @st.cache_data(persist=True)
   def load_data():
-    data = pd.read_csv('~/OneDrive/Desktop/projects/data-projects/ML_web_app/mushrooms.csv')
+    data = pd.read_csv('mushrooms.csv')
     label = LabelEncoder()
     for col in data.columns:
       data[col] = label.fit_transform(data[col])
