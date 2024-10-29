80edd3de1b48a87b76e7e360d59493e16b80f99a
emilyk <emily.g.kruse@gmail.com>
2024-10-29 11:25:48 -0400

    add readme and requirements files

--- ML_app.py
+++ ML_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import seaborn as sns
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve
-from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
+from sklearn.metrics import confusion_matrix, roc_curve
+from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay
 from sklearn.metrics import precision_score, recall_score
 
 def main():
@@ -105,7 +103,7 @@ def main():
       plot_metrics(metrics)
 
   if st.sidebar.checkbox('Show raw data', False):
-    st.subheader('Iris Data Set (Classification)')
+    st.subheader('Mushroom Data Set (Classification)')
     st.write(df)
 
 if __name__ == '__main__':
